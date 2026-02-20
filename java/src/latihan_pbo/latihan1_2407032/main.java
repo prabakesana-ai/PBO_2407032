88dc99d9f11d30e48a5209e6aa2a36b8dc1fa7bd
Prabakesa <prabakesana@gmail.com>
2026-02-20 14:09:06 +0700

    commit ke 5

--- java/src/latihan_pbo/latihan1_2407032/main.java
+++ java/src/latihan_pbo/latihan1_2407032/main.java
@@ -1,7 +1,17 @@
 package latihan_pbo.latihan1_2407032;
 
+class Cafe{
+    String nama;
+    String alamat;
+}
+
 public class main {
     public static void main(String[] args) {
-        System.out.println("Halooooo");
+        Cafe cafe1 = new Cafe();
+        cafe1.nama = "Halaman";
+        cafe1.alamat = "Indramayu";
+        System.out.println("Praktikum");
+        System.out.println("nama cafenya adalah : " + cafe1.nama);
+        System.out.println("alamatnya di : " + cafe1.alamat);
     }
 }
